dd6d6e1527e37768dfeee2c980b8338eaec8dbfc
Shala Qweghen <serafettinyilmaz04@gmail.com>
2016-09-23 16:18:38 +0300

    Fill users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Profile successfully created!"
+			redirect_to @user
+		else
+			flash.now[:error] =  "Please check your info"
+			render :new
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Profile successfully updated!"
+			redirect_to @user
+		else
+			flash[:error] = "Please check your info!"
+			render :edit
+		end
+	end
+
+	def destroy
+		User.find_by(params[:id]).destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).accept(:username, :email)
+	end
 end
