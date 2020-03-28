fc4d614a9b2faa5319dbe1d34fa02540ea822d24
Serafettin Yilmaz <serafettinyilmaz04@gmai.com>
2020-03-28 16:39:45 +0300

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,82 +10,82 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013081608) do
+ActiveRecord::Schema.define(version: 2016_10_13_081608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "body"
-    t.string   "commentable_type"
-    t.integer  "commentable_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "commenter_id"
-    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "friender_id"
-    t.integer  "friend_id"
-    t.integer  "approve",     default: 0
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
-    t.index ["friender_id"], name: "index_friendships_on_friender_id", using: :btree
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "likable_type"
-    t.integer  "likable_id"
-    t.integer  "liked",        default: 1
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["likable_type", "likable_id"], name: "index_likes_on_likable_type_and_likable_id", using: :btree
-    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
-  end
-
-  create_table "pictures", force: :cascade do |t|
-    t.string   "picture"
-    t.integer  "user_id"
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.string "body"
+    t.string "commentable_type"
+    t.integer "commentable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_pictures_on_user_id", using: :btree
+    t.integer "commenter_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "user_id"
+  create_table "friendships", id: :serial, force: :cascade do |t|
+    t.integer "friender_id"
+    t.integer "friend_id"
+    t.integer "approve", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "picture"
-    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["friender_id"], name: "index_friendships_on_friender_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "likes", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "likable_type"
+    t.integer "likable_id"
+    t.integer "liked", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["likable_type", "likable_id"], name: "index_likes_on_likable_type_and_likable_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "pictures", id: :serial, force: :cascade do |t|
+    t.string "picture"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "picture"
-    t.string   "confirmation_token"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "provider"
+    t.string "uid"
+    t.string "picture"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
